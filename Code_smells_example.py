6509049587b96e82ec0126b9e98bd4c2652efa3a
Indlamudi Meghana <54998820+MEGHU123456@users.noreply.github.com>
2023-10-25 13:25:40 -0700

    Update Code_smells_example.py

--- Code_smells_example.py
+++ Code_smells_example.py
@@ -2,8 +2,7 @@
 def calculate_total_price(items):
     total = 0
     for item in items:
-        if item.quantity > 0 and item.price > 100:
-            total += item.price * 0.9
-        elif item.quantity > 0:
-            total += item.price * 0.95
+        if item.quantity > 0:
+            discount = 0.9 if item.price > 100 else 0.95
+            total += item.price * discount
     return total
